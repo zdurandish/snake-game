fb30ae0fee64302f17e4f482411972eda1b1ccfa
zahra <z.durandish52@gmail.com>
2025-02-20 05:07:46 -0800

    new x, y cor defined

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ for position in positions:
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(10)
-
-
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
 
 screen.exitonclick()
