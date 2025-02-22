3360ae74543a04997de19735315b9ace70caca81
zahra <z.durandish52@gmail.com>
2025-02-22 12:32:24 -0800

    collision conditions defined

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -10,6 +12,9 @@ screen.tracer(0)
 game_is_on = True
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -20,5 +25,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.score_record()
 
 screen.exitonclick()
