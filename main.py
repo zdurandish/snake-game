5dfcbd3e964648d1de534d46ab3c63c2b421b21e
zahra <z.durandish52@gmail.com>
2025-02-22 13:03:59 -0800

    wall collision conditions defined

--- main.py
+++ main.py
@@ -31,4 +31,9 @@ while game_is_on:
         food.refresh()
         scoreboard.score_record()
 
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 screen.exitonclick()
