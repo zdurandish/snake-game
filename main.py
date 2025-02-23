b2285001e8aba32695a53b594c8682f177b1dfe7
zahra <z.durandish52@gmail.com>
2025-02-23 09:50:24 -0800

    collision with tail conditions defined

--- main.py
+++ main.py
@@ -29,11 +29,18 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.score_record()
 
     #Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 295 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detect collision with its own tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
