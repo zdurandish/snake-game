ce039184595b6632712bbd16c11c0b943f60b5a8
zahra <z.durandish52@gmail.com>
2025-03-01 06:56:15 -0800

    snake reset method added

--- snake.py
+++ snake.py
@@ -24,7 +24,14 @@ class Snake:
             new_segment.color("white")
             new_segment.goto(position)
             self.segments.append(new_segment)
-    
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
@@ -33,7 +40,7 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
         if self.head.heading() != DOWN:
