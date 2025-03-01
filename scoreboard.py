109cd13ff4d4caa8d6e7c639310673b34a6cc21d
zahra <z.durandish52@gmail.com>
2025-03-01 06:56:02 -0800

    scoreboard reset method added

--- scoreboard.py
+++ scoreboard.py
@@ -7,18 +7,25 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
         self.hideturtle()
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=("Arial", 24, "normal"))
-    
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
     def score_record(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
+        
         
